173ebea788bbbe623b5adf18747e9693e8405158
seawee65 <seawee65@live.com>
2016-07-10 11:44:53 -0400

    changed to be generic and configurable

--- src/com/normarthehero/plugin/mytextcreative/JoinCommand.java
+++ src/com/normarthehero/plugin/mytextcreative/JoinCommand.java
@@ -1,55 +1,37 @@
 package com.normarthehero.plugin.mytextcreative;
 
-import mkremins.fanciful.FancyMessage;
-
-import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+
+import mkremins.fanciful.FancyMessage;
 
 public class JoinCommand extends InfoCommand {
 
-	private String boldWhite = ChatColor.WHITE.toString() + ChatColor.BOLD.toString();
-	private String boldGreen = ChatColor.GREEN.toString() + ChatColor.BOLD.toString();
+		FancyMessage message;
 
-	FancyMessage plotme = new FancyMessage(">> ").style(ChatColor.BOLD).color(ChatColor.GOLD)
-			.then("To start building, type or click ").style(ChatColor.BOLD).color(ChatColor.DARK_AQUA)
-			.then("/plotme auto").command("/plotme auto").tooltip("gives you a plot!").style(ChatColor.BOLD).color(ChatColor.AQUA)
-			.then("!").style(ChatColor.BOLD).color(ChatColor.DARK_AQUA);
+		public JoinCommand(FancyMessage message) {
+			this.message = message;
 
-	FancyMessage donate = new FancyMessage(">> ").style(ChatColor.BOLD).color(ChatColor.GOLD)
-			.then("Donate: ").style(ChatColor.BOLD).color(ChatColor.GREEN)
-			.then("/shop").command("/shop").tooltip("how you can get ranks and other goodies!").style(ChatColor.BOLD).color(ChatColor.AQUA);
+		}
 
-	FancyMessage member = new FancyMessage(">> ").style(ChatColor.BOLD).color(ChatColor.GOLD)
-			.then("Member: ").style(ChatColor.BOLD).color(ChatColor.GREEN)
-			.then("/member").command("/member").tooltip("become a member!").style(ChatColor.BOLD).color(ChatColor.AQUA);
+		FancyMessage plotme = new FancyMessage("§6§l>> §3§lTo start building, type or click ").then("§b§l/plotme auto").command("/plotme auto").tooltip("gives you a plot!");
 
-	FancyMessage faq = new FancyMessage(">> ").style(ChatColor.BOLD).color(ChatColor.GOLD)
-			.then("Info, ranks, minigames, etc: ").style(ChatColor.BOLD).color(ChatColor.DARK_AQUA)
-			.then("/faq").command("/faq").style(ChatColor.BOLD).color(ChatColor.AQUA).tooltip("frequently asked questions and more!");
+		FancyMessage faq = new FancyMessage("§6§l>> §3§lInfo, ranks, minigames, etc: ").then("§b§l/faq").command("/faq").tooltip("frequently asked questions and more!");
 
-	FancyMessage vote = new FancyMessage(">> ").style(ChatColor.BOLD).color(ChatColor.GOLD)
-			.then("Vote for 30 minutes WorldEdit: ").style(ChatColor.BOLD).color(ChatColor.GREEN)
-			.then("/vote").command("/vote").style(ChatColor.BOLD).color(ChatColor.AQUA).tooltip("vote to get worldedit time!");
+		FancyMessage donate = new FancyMessage("§6§l>> §a§lDonate: ").then("§b§l/shop").command("/shop").tooltip("how you can get ranks and other goodies!");
 
-	FancyMessage website = new FancyMessage(" Website: ").style(ChatColor.BOLD).color(ChatColor.GREEN)
-			.then("www.monkeygamesmc.com").link("http://www.monkeygamesmc.com/").tooltip("stay updated!").style(ChatColor.BOLD).color(ChatColor.AQUA);
+		FancyMessage member = new FancyMessage("§6§l>> §a§lMember: ").then("§b§l/member").command("/member").tooltip("become a member!");
 
-	@Override
-	public void sendInfo(CommandSender sender) {
-		sender.sendMessage("");
-		sender.sendMessage(boldWhite + " Welcome to MonkeyCraft Creative!");
-		sender.sendMessage(boldWhite + " Survival IP: life.monkeygamesmc.com");
-		sender.sendMessage("");
-		plotme.send(sender);
-		donate.send(sender);
-		member.send(sender);
-		faq.send(sender);
-		vote.send(sender);
-		sender.sendMessage("");
-		sender.sendMessage(boldGreen + " To stay updated, visit the homepage regulary!");
-		website.send(sender);
-		sender.sendMessage("");
+		FancyMessage vote = new FancyMessage("§6§l>> §a§lVote for 30 minutes of WorldEdit: ").then("§b§l/vote").command("/vote").tooltip("vote to get worldedit time!");
 
-	}
+		FancyMessage website = new FancyMessage("§a§l Website: ").then("§b§lwww.monkeygamesmc.com").link("http://www.monkeygamesmc.com/").tooltip("stay updated!");
+
+		@Override
+		public void sendInfo(CommandSender sender) {
+
+			message.send(sender);
+
+			// sender.sendMessage(""); sender.sendMessage("§l Welcome to MonkeyCraft Creative!"); sender.sendMessage("§l Survival IP: life.monkeygamesmc.com"); sender.sendMessage(""); plotme.send(sender); faq.send(sender); donate.send(sender); member.send(sender); vote.send(sender); sender.sendMessage(""); sender.sendMessage("§a§l To stay updated, visit the homepage regulary!"); website.send(sender); sender.sendMessage("");
+
+		}
 
 }
--- src/com/normarthehero/plugin/mytextcreative/MonkeyPlugin.java
+++ src/com/normarthehero/plugin/mytextcreative/MonkeyPlugin.java
@@ -1,5 +1,7 @@
 package com.normarthehero.plugin.mytextcreative;
 
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -8,57 +10,88 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import mkremins.fanciful.FancyMessage;
+
 public class MonkeyPlugin extends JavaPlugin implements Listener {
 
-	ChatWatcher chatWatcher;
+		ChatWatcher chatWatcher;
 
-	@Override
-	public void onEnable() {
+		@Override
+		public void onEnable() {
 
-		SoundsCommand sounds = new SoundsCommand();
+			saveDefaultConfig();
 
-		chatWatcher = new ChatWatcher();
+			SoundsCommand sounds = new SoundsCommand();
 
-		// register event listeners
-		getServer().getPluginManager().registerEvents(chatWatcher, this);
-		getServer().getPluginManager().registerEvents(sounds, this);
-		getServer().getPluginManager().registerEvents(this, this);
+			chatWatcher = new ChatWatcher();
 
-		// sets up commands
-		getCommand("sounds").setExecutor(sounds);
-		getCommand("join").setExecutor(new JoinCommand());
-		getCommand("shop").setExecutor(new ShopCommand());
-		getCommand("website").setExecutor(new WebsiteCommand());
-		getCommand("member").setExecutor(new MemberCommand());
-		getCommand("faq").setExecutor(new FAQCommand());
+			// register event listeners
+			getServer().getPluginManager().registerEvents(chatWatcher, this);
+			getServer().getPluginManager().registerEvents(sounds, this);
+			getServer().getPluginManager().registerEvents(this, this);
 
-		// save config
-		this.saveDefaultConfig();
+			// sets up commands
+			getCommand("sounds").setExecutor(sounds);
+			getCommand("join").setExecutor(new JoinCommand(getMessage(getConfig().getStringList("welcome"))));
+			getCommand("shop").setExecutor(new ShopCommand());
+			getCommand("website").setExecutor(new WebsiteCommand());
+			getCommand("member").setExecutor(new MemberCommand());
+			getCommand("faq").setExecutor(new FAQCommand());
 
-	}
+			// save config
+			this.saveDefaultConfig();
 
-	@EventHandler
-	public void onJoin(PlayerJoinEvent e) {
+		}
 
-		final Player p = e.getPlayer();
+		// takes config strings and turns into fancymessage
+		public FancyMessage getMessage(List<String> strings) {
 
-		Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+			FancyMessage message = new FancyMessage("");
 
-			@Override
-			public void run() {
+			for (String string : strings) {
 
-				p.performCommand("join");
+					if (string.startsWith("{")) {
+						message.command(string.replaceFirst("\\{", ""));
+
+					} else if (string.startsWith("[")) {
+						message.tooltip(string.replaceFirst("\\[", ""));
+
+					} else if (string.startsWith("@")) {
+						message.link(string.replaceFirst("@", ""));
+
+					} else {
+						message.then(string);
+
+					}
 
 			}
 
-		}, 25L);
+			return message;
 
-	}
+		}
 
-	@EventHandler
-	public void onLeave(PlayerQuitEvent e) {
-		chatWatcher.removePlayer(e.getPlayer().getName());
+		@EventHandler
+		public void onJoin(PlayerJoinEvent e) {
 
-	}
+			final Player p = e.getPlayer();
+
+			Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+
+					@Override
+					public void run() {
+
+						p.performCommand("join");
+
+					}
+
+			}, 25L);
+
+		}
+
+		@EventHandler
+		public void onLeave(PlayerQuitEvent e) {
+			chatWatcher.removePlayer(e.getPlayer().getName());
+
+		}
 
 }
